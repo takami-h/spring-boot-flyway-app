f278fbfc4f94899038a3a6ac193a544d2edb1474
Takami Hirata <26153825+takami-h@users.noreply.github.com>
2023-07-03 16:41:42 +0900

    test: remove unused imports

--- src/test/java/com/redhat/labs/springboot/flyway/adapter/TodoControllerTest.java
+++ src/test/java/com/redhat/labs/springboot/flyway/adapter/TodoControllerTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
