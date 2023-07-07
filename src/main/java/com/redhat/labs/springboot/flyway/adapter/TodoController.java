f51870e769bd95a6c73164b173009942cc6c16af
Takami Hirata <26153825+takami-h@users.noreply.github.com>
2023-07-07 14:31:31 +0900

    feat: remove unused imports

--- src/main/java/com/redhat/labs/springboot/flyway/adapter/TodoController.java
+++ src/main/java/com/redhat/labs/springboot/flyway/adapter/TodoController.java
@@ -1,9 +1,7 @@
 package com.redhat.labs.springboot.flyway.adapter;
 
-import java.time.format.DateTimeParseException;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
