c0e7ad2b66809edb495a829bd3d69e7c4743b256
Takami Hirata <26153825+takami-h@users.noreply.github.com>
2023-07-20 16:57:22 +0900

    test: add assertion patterns

--- src/test/java/com/redhat/labs/springboot/flyway/adapter/TodoControllerTest.java
+++ src/test/java/com/redhat/labs/springboot/flyway/adapter/TodoControllerTest.java
@@ -1,12 +1,14 @@
 package com.redhat.labs.springboot.flyway.adapter;
 
 import static io.restassured.RestAssured.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.Matchers.*;
 
 import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.Optional;
 
+import org.json.JSONArray;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -22,6 +24,7 @@ import com.redhat.labs.springboot.flyway.application.Todo;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 
 @Tag("IT")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -40,12 +43,25 @@ public class TodoControllerTest {
     ((JpaRepository<Todo, Long>) todoRepos).save(Todo.newTodo("家賃を払う", Optional.of(LocalDate.of(2023, 12, 31))));
   }
 
-  @Test void getTodo() {
-    when()
+  @Test void getTodo() throws Exception {
+    var response = when()
       .get("/todos")
     .then()
       .statusCode(HttpStatus.OK.value())
-      .body("title", hasItems("牛乳を買う", "家賃を払う"));
+      .body("title", hasItems("牛乳を買う", "家賃を払う")); // assertion using fluent api
+
+    // or assertion using json
+    var json = response.extract().asString();
+    var todosFetched = new JSONArray(json);
+    assertThat(todosFetched.getJSONObject(0).getString("title")).isEqualTo("牛乳を買う");
+
+    // or JsonPath
+    var todosFetched2 = JsonPath.from(json);
+    assertThat(todosFetched2.getString("[0].title")).isEqualTo("牛乳を買う");
+
+    // or deserialized object
+    var todosFetched3 = response.extract().as(Todo[].class);
+    assertThat(todosFetched3[0].getTitle()).isEqualTo("牛乳を買う");
   }
 
   @Test void postTodo() {
