fb3a855edb8cfcb26f9dc58dae5a6471fcd05500
Takami Hirata <26153825+takami-h@users.noreply.github.com>
2023-12-01 15:36:06 +0900

    bugfix: entity uniqueness

--- src/main/java/com/redhat/labs/springboot/flyway/application/Todo.java
+++ src/main/java/com/redhat/labs/springboot/flyway/application/Todo.java
@@ -61,4 +61,29 @@ public class Todo {
   public void setCreatedAt(LocalDateTime createdAt) {
     this.createdAt = createdAt;
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Todo other = (Todo) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
 }
